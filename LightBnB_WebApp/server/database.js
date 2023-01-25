cc2a5dd91676891c84a27ca899662c4015c13c1a
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-01-25 14:03:29 -0800

    Add getAllReservations to fix query issue

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -68,7 +68,15 @@ exports.addUser = addUser;
 const getAllReservations = function(guest_id, limit = 10) {
 
   return pool.query(
-    'SELECT * FROM reservations JOIN properties ON property_id = properties.id JOIN property_reviews ON property_reviews.property_id = properties.id WHERE reservations.guest_id = $1 GROUP BY properties.id, reservations.id, properties.title, properties.cost_per_night, property_reviews.id ORDER BY start_date LIMIT $2;', [guest_id, limit])
+    `SELECT reservations.id, reservations.start_date, reservations.end_date,
+    properties.*, AVG(property_reviews.rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;`, [guest_id, limit])
     .then((response) => {
       return response.rows;
     });
